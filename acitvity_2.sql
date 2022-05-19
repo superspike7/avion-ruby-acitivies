5e5a7b2087f30df3c5d2446d22e7e0d7d6566f7d
Spike Vinz <spikevinz@gmail.com>
2022-05-19 19:43:40 +0800

    adds sql acitivity 2 solution

---
+++ acitvity_2.sql
@@ -0,0 +1,7 @@
+SELECT COUNT(*) AS total_students FROM students;
+
+SELECT  * FROM students WHERE location = 'Manila';
+
+SELECT AVG(age) AS average_age_of_students FROM students;
+
+SELECT * FROM students ORDER BY age DESC;
